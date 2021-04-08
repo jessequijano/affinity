dc741b72bec6f89f433a3a622a6e1e8bd847bf34
JulioPDX <juliop503@gmail.com>
2021-04-08 13:41:23 -0700

    mostly fixed ref sheet

--- ref_build.py
+++ ref_build.py
@@ -19,19 +19,21 @@ def main():
 with open("reference.md", "w") as my_file:
     my_file.write("# Reference for Icons\n")
     my_file.write("\n")
-    my_file.write("|Name|Icon|Name|Icon|\n")
-    my_file.write("|:-------------|:----------:|-----------:|-----------:|\n")
+    my_file.write("|Name|Icon|Name|Icon|Name|Icon|\n")
+    my_file.write("|:-------------:|:----------:|:-----------:|:-----------:|:-----------:|:-----------:|\n")
     for (root, dirs, files) in walk(PATH):
         count = 0
         for f in files:
             if ".png" in f:
                 count += 1
-                if count % 3 == 0:
-                    my_file.write("|\n")
+                print(count)
+                if count == 1:
+                    my_file.write(f"|{f}|![{f}]({root}/{f})|")
+                if count == 2:
+                    my_file.write(f"{f}|![{f}]({root}/{f})|")
+                if count == 3:
+                    my_file.write(f"{f}|![{f}]({root}/{f})|\n")
                     count = 0
-                else:
-                    my_file.write("|")
-                    my_file.write(f"{f}|![{f}]({root}/{f})")
     my_file.write("|\n")
 
 if __name__ == "__main__":
